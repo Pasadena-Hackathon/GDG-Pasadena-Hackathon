c934d8c99f2829f9a9ed693d2f1bfc6b46d47266
Josh Welliver <joshwelliver@gmail.com>
2024-05-26 00:16:39 -0700

    Layout refactor
    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,54 @@
 "use client";
 
-// import Image from "next/image";
-// import GraphTest from "@/components/GraphTest";
-import KnowledgePage, { testData } from "@/components/VideoContainer";
+import { useState } from "react";
+import KnowledgePage, {
+  testData,
+  TopicData,
+} from "@/components/VideoContainer";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [curData, setCurData] = useState<TopicData[]>([]);
+
+  const onSearchBtnClick = (searchText: string) => {
+    // TODO: SEND Search request for text
+    const msg = { searchText };
+    console.log(msg);
+  };
+
   return (
-    <div className="flex h-screen flex-col items-center bg-slate-900">
-      <SearchBar />
-      <KnowledgePage topics={testData} />
+    <div className="flex flex-col items-center min-h-screen bg-slate-900">
+      <SearchBar isLoading={isLoading} onSearchBtnClick={onSearchBtnClick} />
+      <KnowledgePage topics={testData} isLoading={isLoading} />
     </div>
   );
 }
 
-function SearchBar() {
-  return <input className="text-black" />;
+function SearchBar(props: {
+  isLoading: boolean;
+  onSearchBtnClick: (s: string) => any;
+}) {
+  const [text, setText] = useState<string>("");
+
+  const onClick = () => {
+    if (text.length === 0 || props.isLoading) return;
+    props.onSearchBtnClick?.(text);
+  };
+
+  return (
+    <div className="flex m-4 place-content-center join w-[80%] sm:w-[50%]">
+      <input
+        className="input input-primary input-md join-item focus:outline-none w-full opacity-70"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        placeholder="Search any topic... e.g. How to win a Hackathon"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") onClick();
+        }}
+      />
+      <button className="btn btn-primary join-item" onClick={onClick}>
+        Search
+      </button>
+    </div>
+  );
 }
--- src/components/VideoContainer.tsx
+++ src/components/VideoContainer.tsx
@@ -4,14 +4,55 @@ export interface VideoData {
 }
 
 export interface TopicData {
-  title: string;
+  topic: string;
   description: string;
   videos: VideoData[];
 }
 
-export const testData = [
+export const testData: TopicData[] = [
   {
-    title: "Title 1",
+    topic:
+      "Topic really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really,",
+    description:
+      "some really, really, really, really, really, really, really, really, really, really, really, really,",
+    videos: [
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+    ],
+  },
+  {
+    topic: "Topic 2",
+    description: "some description again",
+    videos: [
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+    ],
+  },
+  {
+    topic: "Topic 1",
     description: "some description",
     videos: [
       {
@@ -20,7 +61,25 @@ export const testData = [
     ],
   },
   {
-    title: "Title 2",
+    topic: "Topic 2",
+    description: "some description again",
+    videos: [
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+    ],
+  },
+  {
+    topic: "Topic 1",
+    description: "some description",
+    videos: [
+      {
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D",
+      },
+    ],
+  },
+  {
+    topic: "Topic 2",
     description: "some description again",
     videos: [
       {
@@ -33,7 +92,7 @@ export const testData = [
 export function VideoContainer(props: { videoData: VideoData }) {
   //todo: thumbnail ?
   return (
-    <div className="p-10 aspect-square border-2 border-red-500">
+    <div className="aspect-square border-2 border-red-500 place-content-center">
       <a href={props.videoData.url}>Goto</a>
     </div>
   );
@@ -43,29 +102,61 @@ export function TopicContainer(props: { topicData: TopicData }) {
   /**
    * Will hold title, summary as to why, and links.
    */
+  //   return (
+  //     <div className="flex flex-row">
+  //       {/* title and desc */}
+  //       <div className="flex flex-col gap-4 border-2 border-purple-500 p-4 items-center">
+  //         <div className="text-xl">{props.topicData.topic}</div>
+  //         <div className="text-sm">{props.topicData.description}</div>
+  //       </div>
+  //       {/* videos */}
+  //       <div className="flex flex-row">
+  //         {props.topicData.videos.map((vid, index) => (
+  //           <VideoContainer key={index} videoData={vid} />
+  //         ))}
+  //       </div>
+  //     </div>
+  //   );
+
   return (
-    <div className="flex flex-row">
-      {/* title and desc */}
-      <div className="flex flex-col gap-4 border-2 border-purple-500">
-        <div className="text-xl">{props.topicData.title}</div>
-        <div className="text-sm">{props.topicData.description}</div>
-      </div>
+    <div className="flex flex-col sm:flex-row border-2">
+      <TopicInfo topicData={props.topicData} />
       {/* videos */}
-      <div className="flex flex-row">
+      <div className="grid grid-flow-row grid-cols-2 sm:grid-cols-3 md:grid-cols-5 items-center auto-rows-auto w-full">
         {props.topicData.videos.map((vid, index) => (
-          <VideoContainer key={index} videoData={vid} />
+          <div
+            className="size-full m-2 border-2 place-content-center"
+            key={index}
+          >
+            <VideoContainer key={index} videoData={vid} />
+          </div>
         ))}
       </div>
     </div>
   );
 }
 
-export default function KnowledgePage(props: { topics: TopicData[] }) {
+function TopicInfo(props: { topicData: TopicData }) {
+  return (
+    <div className="flex flex-col border-2 border-purple-500 p-4 items-start text-left w-full sm:max-w-[30%] min-w-[30%] min-h-20">
+      <div className="text-3xl font-bold opacity-50">
+        {props.topicData.topic}
+      </div>
+      <div className="text-sm font-thin">{props.topicData.description}</div>
+    </div>
+  );
+}
+
+export default function KnowledgePage(props: {
+  topics: TopicData[];
+  isLoading: boolean;
+}) {
   /**
    * contains all TopicContainers
    */
   return (
-    <div className="flex flex-col gap-2 border-2 border-white min-w-full">
+    //
+    <div className="flex flex-col gap-2 border-2 border-white min-w-full max-w-screen">
       {props.topics.map((topicData, index) => (
         <TopicContainer key={index} topicData={topicData} />
       ))}
